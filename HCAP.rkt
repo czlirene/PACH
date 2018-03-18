fc968a2e770ad77a8d35d8526bee3dd2767b38c6
slchan <slchan@XIV.com>
2018-03-17 21:52:44 -0600

    Started defining language

--- HCAP.rkt
+++ HCAP.rkt
@@ -2,4 +2,32 @@
 (require redex)
 (require racket/trace)
 
-(define-language HCAP)
+(define-language HCAP
+    ;;; SA (Permissions, Automaton states, initial state, partial transition)
+    [Perm   ]
+    [PTrans (AState Perm)]
+    []
+    ;;; Protocol states 
+    [Clock  (clock  natural)]
+    [Time   (time   natural))]
+    ;;; ers is the transitioning permissions, should prob be list?
+    [Except (ers    )]
+    [AuthSS (QState Time)]
+    [RsrcSS (Serial )]
+    [Client (Tik ...)]
+    [Tik    Update
+            CapC]
+    ;;; Set of tickets
+    [Update (p t e')]
+    [CapC   (tser, F)]
+
+    [TranID Issue
+            Request
+            Flush
+            Update
+            Recover
+            Drop]
+    [Tiks   (Tik ...)]
+
+    [State  (st )]
+)
